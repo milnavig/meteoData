b29df97a3375b246ac404ad4129998a4d2e92ed8
milnavig <milnavig@gmail.com>
2020-10-17 23:34:43 +0300

    3

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://milnavig:36521908467_5@cluster0.dssrv.mongodb.net/test?retryWrites=true&w=majority'
+    'mongoUrl' : ENV['MONGODB_URI']
 }
