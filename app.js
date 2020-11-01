384f84e23ef7b1b6764ff69ad0e177ce12204dd5
milnavig <milnavig@gmail.com>
2020-11-01 13:00:12 +0200

    9

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -32,6 +33,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
